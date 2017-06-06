1924764fe2fdcd82cf3edb22c2c3a2d25bf94f6d
Sudeb Sarkar <Sudeb@Sudebs-MacBook-Air.local>
2017-06-06 08:20:44 +0530

    Uploade first

---
+++ WebServiceCallEx1/WebServiceCallEx1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebServiceCallEx1
+//
+//  Created by Sudeb Sarkar on 06/06/17.
+//  Copyright © 2017 test ss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
